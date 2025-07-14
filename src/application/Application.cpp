1b76554322fa45d01975e000b0651a9bcf4f6648
Gallon Zhou <gallonzhou04@gmail.com>
2025-07-14 23:54:03 +1200

    Feat: Added Application Setup

---
+++ src/application/Application.cpp
@@ -0,0 +1,104 @@
+#include "external/glad/glad.h"
+
+#include "application/Application.hpp"
+
+#include "logger/LoggerMacros.hpp"
+
+#include <iostream>
+
+namespace application {
+
+Application::Application() : _lastTime(0.0f), _framesPerSecond(0.0f) {
+}
+
+void Application::initialise() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+#endif
+
+    GLFWmonitor *monitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode *mode = glfwGetVideoMode(monitor);
+
+    glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+    glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+    glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+    glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+
+    GLFWwindow *window = glfwCreateWindow(mode->width, mode->height, "Quaternion", monitor, nullptr);
+
+    if (window == nullptr) {
+        std::cout << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialise GLAD" << std::endl;
+
+        return;
+    }
+
+    glfwSetWindowUserPointer(window, this);
+
+    glfwSetFramebufferSizeCallback(window, [](GLFWwindow *window, int width, int height) {
+        Application *application = static_cast<Application *>(glfwGetWindowUserPointer(window));
+
+        application->onResize(window, width, height);
+    });
+
+    this->_window = window;
+}
+
+void Application::load() {
+    int width;
+    int height;
+
+    glfwGetFramebufferSize(this->_window, &width, &height);
+
+    this->setWindowSize(width, height);
+}
+
+void Application::run() {
+    glfwSwapInterval(0);
+
+    while (!glfwWindowShouldClose(this->_window)) {
+        this->update();
+
+        this->render();
+
+        glfwSwapBuffers(this->_window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+}
+
+void Application::update() {
+    float time = glfwGetTime();
+
+    float deltaTime = time - this->_lastTime;
+}
+
+void Application::render() {
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void Application::onResize(GLFWwindow *window, int width, int height) {
+    this->setWindowSize(width, height);
+}
+
+void Application::setWindowSize(int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+} // namespace application
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "application/Application.hpp"
+
+using namespace application;
+
+int main() {
+    Application application;
+
+    application.initialise();
+
+    application.load();
+
+    application.run();
+
+    return 0;
+}
